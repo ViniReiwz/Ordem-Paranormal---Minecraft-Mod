6346d6311f17efce81d711ce9787e45839480c3a
ViniReiwz <vinireisg@usp.br>
2026-02-04 16:48:58 -0300

    bugou tudo

--- src/main/java/com/example/greisvini/ordem_paranormal/network/packets/ExampleC2SPacket.java
+++ src/main/java/com/example/greisvini/ordem_paranormal/network/packets/ExampleC2SPacket.java
@@ -3,9 +3,12 @@ package com.example.greisvini.ordem_paranormal.network.packets;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
+
+import com.example.greisvini.ordem_paranormal.capabilities.attributes.AtributosProvider;
 
 public class ExampleC2SPacket 
 {
